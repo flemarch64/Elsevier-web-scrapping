ab48836a7a14d42b311526fea4c15d0c83d2155d
flemarch64 <76454791+flemarch64@users.noreply.github.com>
2022-06-06 13:16:37 +0200

    Update search_scidir.py

--- search_scidir.py
+++ search_scidir.py
@@ -1,19 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 27 17:00:14 2021
+#This script creates a data frame from search results.
 
-@author: flemarchand
-
-This script creates a data frame from search results.
-"""
 import requests
 
 import pandas as pd
-#Number of times cited (scopus)
+#Enter your own elsevier API key as a string
 apiKey2=
+#URL needed to make the json request
 url3="https://api.elsevier.com/content/search/scopus"
-params1=dict(query="CFD",sort="citedby-count",apiKey=apiKey2,count="150",httpAccept="application/json")
+#Enter your search keyword
+query1="CFD"
+#Criterion to order the results
+sort1="citedby-count"
+#Number of results stored in the data frame
+count1="150"
+#Parameter dictionnary needed to make the json request
+params1=dict(query=query1,sort=sort1,apiKey=apiKey2,count=count1,httpAccept="application/json")
+#Making the request
 test=requests.get(url=url3,params=params1)
+#Json request
 data=test.json()
+#Create the data frame columns
 df=data["search-results"]["entry"]
+
+#Final data frame generated
 df1=pd.DataFrame(df)
